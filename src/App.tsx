2881ec9b3102014be75d0ea8d4b6295760857d5c
citrus327 <citrus327@outlook.com>
2023-02-14 19:20:15 +0800

    feat: with dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(1);
@@ -9,6 +9,16 @@ function App() {
   return (
     <div>
       FRONT PAGE
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">home</Link>
+          </li>
+          <li>
+            <Link to="/about">about</Link>
+          </li>
+        </ul>
+      </nav>
       <button onClick={() => setCount((v) => v + 1)}>{count}</button>
       <Routes>
         <Route path="/" element={<Home />} />
